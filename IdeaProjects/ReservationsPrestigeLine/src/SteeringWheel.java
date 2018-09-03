da42b3c52fdc98a3ce957d3325d2c64beeee1d01
Sebastian <sebastiandabek99@gmail.com>
2018-09-03 21:56:14 +0200

    Main

---
+++ IdeaProjects/ReservationsPrestigeLine/src/SteeringWheel.java
@@ -0,0 +1,10 @@
+public class SteeringWheel {
+
+    public static void main (String args[]){
+
+        // Placeholder of main User Interface
+        System.out.print("Prestige Line Reservations System");
+
+    }
+
+}
