c4be0976bcaaa3125661b20427934807a9b29c9a
sebekdabek <sebekdabek@gmail.com>
2018-09-04 14:41:07 +0200

    Orders, spelling in capacity, setter corrected

--- IdeaProjects/ReservationsPrestigeLine/src/CustomersAndTransfers.java
+++ IdeaProjects/ReservationsPrestigeLine/src/CustomersAndTransfers.java
@@ -4,17 +4,46 @@ public class CustomersAndTransfers {
 
     private ArrayList<Voucher> vouchers;
     private ArrayList<Customer> customers;
-    private ArrayList<Capacity> capacitiy;
+    private ArrayList<Capacity> capacity;
     private ArrayList<Transfer> transfers;
 
     public CustomersAndTransfers() {
 
         vouchers = new ArrayList<Voucher>();
         customers = new ArrayList<Customer>();
-        capacitiy = new ArrayList<Capacity>();
+        capacity = new ArrayList<Capacity>();
         transfers = new ArrayList<Transfer>();
 
-
     }
+
+    public ArrayList<Voucher> getVouchers() {
+        return vouchers;
+    }
+
+
+    public ArrayList<Customer> getCustomers() {
+        return customers;
+    }
+
+
+    public ArrayList<Capacity> getCapacitiy() {
+        return capacity;
+    }
+
+
+    public ArrayList<Transfer> getTransfers() {
+        return transfers;
+    }
+
+    public void addCapacity(String capacityPlace) {
+        capacity.add(new Capacity(capacityPlace));
+    }
+
+    public void addTransfer( int transferNumber, String startPoint, String endPoint  ){
+
+        transfers.add(new Transfer(transferNumber));
+    }
+
+
 
 }
