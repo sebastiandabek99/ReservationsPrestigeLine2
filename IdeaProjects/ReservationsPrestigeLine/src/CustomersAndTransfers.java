dac0d269e7877b21c6f8acca55663e8a1d78a6d1
sebekdabek <sebekdabek@gmail.com>
2018-09-04 12:51:36 +0200

    Orders

---
+++ IdeaProjects/ReservationsPrestigeLine/src/CustomersAndTransfers.java
@@ -0,0 +1,20 @@
+import java.util.ArrayList;
+
+public class CustomersAndTransfers {
+
+    private ArrayList<Voucher> vouchers;
+    private ArrayList<Customer> customers;
+    private ArrayList<Capacity> capacitiy;
+    private ArrayList<Transfer> transfers;
+
+    public CustomersAndTransfers() {
+
+        vouchers = new ArrayList<Voucher>();
+        customers = new ArrayList<Customer>();
+        capacitiy = new ArrayList<Capacity>();
+        transfers = new ArrayList<Transfer>();
+
+
+    }
+
+}
